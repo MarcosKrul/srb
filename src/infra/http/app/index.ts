21f11f8731f67d748d2bbca7ca708e1cff69f7f0
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-09-23 23:18:05 -0300

    fix(infra): fix middlewares

--- src/infra/http/app/index.ts
+++ src/infra/http/app/index.ts
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 app.use(errorHandler);
 
 process.on("SIGTERM", () => {
